38cc157814a42e22fe337e0852f975719db0708e
Vitaly Kostikov <one.kostikov@gmail.com>
2021-12-24 13:26:15 +0100

    Initial Commit

---
+++ SignOnGo/SignOnGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignOnGoApp.swift
+//  SignOnGo
+//
+//  Created by Vitaly on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SignOnGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
